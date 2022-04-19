2dca4e22eccd2927f9f18ecc4532700062f2a653
Árki <95213390+BIGSHOT04@users.noreply.github.com>
2022-04-19 14:47:47 +0200

    cement1

---
+++ OpeningWeekend/film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpeningWeekend
+{
+    internal class film
+    {
+        public string OGCim { get; set; }
+        public string magyarcim { get; set; }
+        public string Bemutato { get; set; }
+        public string forgalmazo { get; set; }
+        public int Bevétel { get; set; }
+        public int látogatok { get; set; }
+    }
+}
