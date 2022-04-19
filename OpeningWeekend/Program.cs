aa88477ef50e70a64ceb36937cccd5382ef1bb82
Árki <95213390+BIGSHOT04@users.noreply.github.com>
2022-04-19 15:08:39 +0200

    cement5?

--- OpeningWeekend/Program.cs
+++ OpeningWeekend/Program.cs
@@ -21,7 +21,15 @@ namespace OpeningWeekend
             }
             Console.WriteLine($"Filemk az állományban:{films.Count}db");
 
-
+            long uip=0;
+            foreach (var f in films)
+            {
+                if (f.forgalmazo=="UIP")
+                {
+                    uip += f.Bevétel;
+                }
+            }
+            Console.WriteLine($"Az uip duna film forgalmazó elsö heti bevétele: {uip.ToString("C2")}");
 
 
 
